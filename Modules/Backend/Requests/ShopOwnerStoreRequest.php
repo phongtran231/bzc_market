efaee90b23cc7e70a335a4f847fb91d9d3dfffa9
Quý Phạm <yungbloodx02@gmail.com>
2020-09-22 21:36:44 +0700

    Created Shopowner

---
+++ Modules/Backend/Requests/ShopOwnerStoreRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Modules\Backend\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ShopOwnerStoreRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'full_name' => [
+                'string',
+                'max:100'
+            ],
+            'phone' => [
+                'required',
+                'regex:/^0+\d{9}$/m',
+                'unique:shop_owners,phone'
+            ],
+            'email' => [
+                'required',
+                'email',
+                'unique:shop_owners,email'
+            ],
+            'address' => [
+                'string'
+            ],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'full_name.string' => 'Vui lòng nhập đúng họ tên',
+            'full_name.max' => 'Họ và tên giới hạn 100 ký tự',
+            'phone.required' => 'Số điện thoại cần khai báo',
+            'phone.numeric' => 'Số điện thoại chỉ nhập số',
+            'phone.unique' => 'Đã có người đăng ký bằng số điện thoại này',
+            'phone.regex' => 'Số điện thoại sai định dạng',
+            'email.required' => 'Vui lòng điền Email',
+            'email.email' => 'Vui lòng nhập đúng email. Eg: john@example.com',
+            'email.unique' => 'Email đã có người đăng ký',
+        ];
+    }
+}
