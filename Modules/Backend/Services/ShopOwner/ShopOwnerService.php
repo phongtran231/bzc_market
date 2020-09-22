7b39896d8fe2ff68c9b632d7de985d78a51b2aaa
Quý Phạm <yungbloodx02@gmail.com>
2020-09-22 21:01:19 +0700

    defined methods

--- Modules/Backend/Services/ShopOwner/ShopOwnerService.php
+++ Modules/Backend/Services/ShopOwner/ShopOwnerService.php
@@ -3,8 +3,14 @@
 namespace Modules\Backend\Services\ShopOwner;
 
 use App\Repositories\ShopOwner\ShopOwnerRepositoryInterface;
+use Illuminate\Support\Facades\Request;
 use Modules\Backend\Services\BaseService;
 
+/**
+ * Class ShopOwnerService
+ * @package Modules\Backend\Services\ShopOwner
+ * @author S5K (s5k.github.io)
+ */
 class ShopOwnerService extends BaseService implements ShopOwnerServiceInterface
 {
     /**
@@ -17,5 +23,37 @@ class ShopOwnerService extends BaseService implements ShopOwnerServiceInterface
     )
     {
         $this->shopOwnerRepository = $shopOwnerRepository;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getInfo()
+    {
+        return $this->shopOwnerRepository->find(auth()->guard('api')->id());
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function update(Request $attrs)
+    {
+
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function save(Request $attrs)
+    {
+        // TODO: Implement save() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function destroy(int $id)
+    {
+        // TODO: Implement destroy() method.
     }
 }
--- Modules/Backend/Services/ShopOwner/ShopOwnerServiceInterface.php
+++ Modules/Backend/Services/ShopOwner/ShopOwnerServiceInterface.php
@@ -2,7 +2,32 @@
 
 namespace Modules\Backend\Services\ShopOwner;
 
+use App\Models\ShopOwner;
+use Illuminate\Support\Facades\Request;
+
 interface ShopOwnerServiceInterface
 {
+    /**
+     * @return ShopOwner
+     */
+    public function getInfo();
+
+    /**
+     * @param Request $attrs
+     * @return void
+     */
+    public function update(Request $attrs);
+
+    /**
+     * @param Request $attrs
+     * @return ShopOwner
+     */
+    public function save(Request $attrs);
+
+    /**
+     * @param int $id
+     * @return void
+     */
+    public function destroy(int $id);
 
 }
